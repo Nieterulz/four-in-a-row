dfccb0ed7020e21cb98316447cc61b96c78a6eeb
Nieterulz <ajsamu@hotmail.com>
2020-07-19 20:45:46 +0200

    "feat: making tic-tac-toe"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from "react";
-import Fourinarow from "./components/Fourinarow";
 
 export default function App(props) {
-    return (
-        <div>
-            <header>4 in a row</header>
-            <Fourinarow />
-        </div>
-    );
+    return <div></div>;
 }
